50845729d5cf58573360db6d2d7f0b08a2457afe
primonmari <114789403+primonmari@users.noreply.github.com>
2024-03-21 22:26:43 -0300

    projeto

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Text>Login</Text>
+      <TextInput> </TextInput>
+      <Text>Senha</Text>
+      <TextInput> </TextInput>
+      <Button title='Salvar'></Button>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
